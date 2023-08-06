11b54207ebfc8ba1f5e1ef90a511eee9dbd4f7ab
Kile K Asmussen <kile.asmussen@gmail.com>
2023-08-07 00:20:09 +0200

    .

--- src/general_proxy.rs
+++ src/general_proxy.rs
@@ -1,10 +1,20 @@
-
 trait GeneralProxyBuilder {
-
-    fn 
-
     fn name(&mut self, name: &str) -> &mut Self;
-    fn types(&mut self, super_type: &str, sub_type: Option<&str>) -> &mut Self;
+    fn types(&mut self, type_line: &str) -> &mut Self;
 
+    fn transform<F>(&mut self, notice: Option<&str>, back: F) -> &mut Self
+    where
+        F: FnOnce(&mut Self);
 
+    fn double_sided<F>(&mut self, back: F) -> &mut Self
+    where
+        F: FnOnce(&mut Self);
+
+    fn format(&mut self, layout: ArtLayout) -> &mut Self;
+}
+
+enum ArtLayout {
+    Ordinary,
+    VerticalArtLeft,
+    VeritcalArtRight,
 }
